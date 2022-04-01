1324b1342904dff81f4458b572c22708c55f159c
aviladiogo <aviladiogo30@gmail.com>
2022-03-31 22:32:17 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello!')
+console.log('hello!');
+
+
+var real = document.querySelector('#Real').value
+
+function Disable(){
+  document.getElementById("botao1").disabled = true;
+  document.getElementById("botao2").disabled = false;
+  document.getElementById("botao3").disabled = false;
+  document.getElementById("botao4").disabled = false;
+
+}
+
+function DtoR() {
+  var taxa = document.querySelector('#Taxa').value
+  var dolar = document.querySelector('#Dolar').value
+  var resultadoD = document.getElementById('name')
+  resultadoD.innerHTML = parseFloat(dolar )* parseFloat(taxa)
+
+}
+function RtoD() {
+  var taxa = document.querySelector('#Taxa').value
+  var real = document.querySelector('#Real').value
+  var resultadoR = document.getElementById('name2')
+  resultadoR.innerHTML = parseFloat(real )/ parseFloat(taxa)
+}
+function Reset(){
+  document.getElementById("botao1").disabled = false;
+  document.getElementById("botao2").disabled = true;
+  document.getElementById("botao3").disabled = true;
+  document.getElementById("botao4").disabled = true;
+
+  var resultadoD = document.getElementById('name')
+  resultadoD.innerHTML = null
+
+  var resultadoR = document.getElementById('name2')
+  resultadoR.innerHTML = null
+}
